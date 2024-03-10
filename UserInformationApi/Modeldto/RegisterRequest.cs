0da543810872bbd364b09723391f65737b32827b
Mona Basu <mbasu490@gmail.com>
2024-03-09 23:33:11 -0800

    modeldto added

--- UserInformationApi/Modeldto/RegisterRequest.cs
+++ UserInformationApi/Modeldto/RegisterRequest.cs
@@ -5,7 +5,7 @@
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+         
         public string? Gender { get; set; }
 
         public DateOnly? DateOfBirth { get; set; }
